3e2032f1a0773520f40a8b1f832d7d821c81c681
ghassanelgendy <112765677+ghassanelgendy@users.noreply.github.com>
2023-05-05 15:28:33 +0300

    Committing files

---
+++ CodeForces/GNU C++20 (64)/343B | Alternating Current/204513372.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+ 
+int main() {
+    string wires;
+    cin >> wires;
+    
+    stack<char> s;
+    for (int i = 0; i < wires.length(); i++) {
+        if (!s.empty() && s.top() == wires[i]) {
+            s.pop();
+        } else {
+            s.push(wires[i]);
+        }
+    }
+    
+    if (s.empty()) {
+        cout << "Yes" << endl;
+    } else {
+        cout << "No" << endl;
+    }
+    
+    return 0;
+}
