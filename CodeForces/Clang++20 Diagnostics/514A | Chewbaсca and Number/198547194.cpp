b872c9b65404719f3e8356e0f97c9206ddf692d7
ghassanelgendy <112765677+ghassanelgendy@users.noreply.github.com>
2023-05-05 15:29:45 +0300

    Committing files

---
+++ CodeForces/Clang++20 Diagnostics/514A | Chewbaсca and Number/198547194.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+    string x;
+    cin >> x;
+    for (int i = 0; i < x.size() ; ++i) {
+        if (x[i]>= '5' && x[0] != '9' || x[i]>= '5' && i != 0)
+            x[i] = '9' - x[i] + '0';
+    }
+    cout << x;
+}
